f017c03566e0a5cb4ffe5d4cc4c60294f75579b5
Majid Najafov <majidan@code.edu.az>
2026-03-10 12:07:19 +0400

    commit

--- components/HeroSection/types.ts
+++ components/HeroSection/types.ts
@@ -1,3 +1,5 @@
 export type Rect = { x: number; y: number; w: number; h: number; r: number };
 
 export type CardKey = 'payment' | 'champion' | 'cart' | 'shop3d' | 'supreme';
+
+export type RectMap = Partial<Record<CardKey, Rect>>;
